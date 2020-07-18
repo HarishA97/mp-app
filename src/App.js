a3b9b620d786fdc10fd404dfa4688e4b3d059b11
Harish A <harisha.a33@gmail.com>
2020-07-18 19:18:52 +0530

    Display local time string instead of utc

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {password: new Date().toUTCString()};
+    this.state = {password: new Date().toString()};
   }
 
   render() {
