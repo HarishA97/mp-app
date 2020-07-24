d318f6d86460d5c53d6e6894eec8641edf683251
Harish A <harisha.a33@gmail.com>
2020-07-24 19:29:10 +0530

    Adds paths

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Reroute from './reroute';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Reroute />
   </React.StrictMode>,
   document.getElementById('root')
 );
